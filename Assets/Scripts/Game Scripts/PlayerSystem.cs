fa9039c44a17bc5531440d20d0aab7f52ec44178
Alpha 2 <8784iirv5@gmail.com>
2023-11-03 16:30:16 +0100

    Player System

--- Assets/Scripts/Game Scripts/PlayerSystem.cs
+++ Assets/Scripts/Game Scripts/PlayerSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSystem : MonoBehaviour
 {
@@ -10,6 +11,19 @@ public class PlayerSystem : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI _notificationMessage;
+
+    //upgrades buttons
+    [SerializeField]
+    private Button _2xMoneyButton;
+    [SerializeField]
+    private Button _4xMoneyButton;
+
+    // upgrades buttons text
+    [SerializeField] 
+    private TextMeshProUGUI _2xMoneyButtonMessage;
+
+    [SerializeField] 
+    private TextMeshProUGUI _4xMoneyButtonMessage;
 
     public int PlayerMoney { get; set; }
 
@@ -27,17 +41,32 @@ public class PlayerSystem : MonoBehaviour
     {
         _moneyCounter.text = PlayerMoney.ToString() + "$";
 
+        // upgrades buttons text
+        _2xMoneyButtonMessage.text = "2x money\nboost\n(10$)";
+        _4xMoneyButtonMessage.text = "4x money\nboost\n(20$)";
+
         // save progress
         PlayerPrefs.SetInt("money", PlayerMoney);
         PlayerPrefs.SetInt("boost2xmoney", _2xMoneyboost);
         PlayerPrefs.SetInt("boost4xmoney", _4xMoneyboost);
+
+        // upgrades button interactable check
+        if (_2xMoneyboost != 0)
+        {
+            _2xMoneyButton.interactable = false;
+        }
+
+        if (_4xMoneyboost != 0)
+        {
+            _4xMoneyButton.interactable = false;
+        }
     }
 
     public void On2xMoneyBuy()
     {
         if (PlayerMoney >= _2xmoneyCost && _2xMoneyboost == 0)
         {
-            _moneyBonus = 2;
+            _moneyBonus += 1;
             _2xMoneyboost++;
             PlayerMoney -= _2xmoneyCost;
         }
@@ -50,10 +79,10 @@ public class PlayerSystem : MonoBehaviour
 
     public void On4xMoneyBuy()
     {
-        if (PlayerMoney >= _4xmoneyCost && _2xMoneyboost == 0)
+        if (PlayerMoney >= _4xmoneyCost && _4xMoneyboost == 0)
         {
-            _moneyBonus = 4;
-            _2xMoneyboost++;
+            _moneyBonus += 4;
+            _4xMoneyboost++;
             PlayerMoney -= 20;
         }
         else if (PlayerMoney < 10)
